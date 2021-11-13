86fe5995d2d0e41405c45d42db60d094c119e3ed
kosintop <kosin@outlook.com>
2021-11-13 20:55:05 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-facebook-login-example-pqvs3u
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-facebook-login-example-pqvs3u)
---
+++ env.js
@@ -0,0 +1,5 @@
+// moved env variables into js file for stackblitz because it doesn't support dotenv (.env)
+export default {
+  REACT_APP_API_URL: 'http://localhost:4000',
+  REACT_APP_FACEBOOK_APP_ID: '4655629124493784',
+};
